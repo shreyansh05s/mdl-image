794d56c636967ca7e930fb34305342a063ed723f
Shreyansh Sharma <shreyansh05s@gmail.com>
2023-05-13 17:34:02 +0200

    Fixed batching for align

--- align_eval.py
+++ align_eval.py
@@ -3,33 +3,68 @@ import torch
 from PIL import Image
 from transformers import AlignProcessor, AlignModel
 from torchvision.datasets import CIFAR10, CIFAR100
+from tqdm.auto import tqdm
+import json
+from torch.utils.data import DataLoader
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 processor = AlignProcessor.from_pretrained("kakaobrain/align-base")
 model = AlignModel.from_pretrained("kakaobrain/align-base")
 
-test_dataset = CIFAR100(root='./data', train=False, download=True)
+model.to(device)
+model.eval()
 
-# test_dataloader = DataLoader(test_dataset, batch_size=16, shuffle=True, num_workers=4, pin_memory=True)
+with open("cifar100_classes.json", "r") as f:
+    classes = json.load(f)
 
-random_int = torch.randint(0, len(test_dataset), (1,)).item()
 
-sample = test_dataset[random_int]
-image = sample[0]
+def transform_image_ALIGN(image):
+    image = processor(images=image, return_tensors="pt")
+    image = {k: v.squeeze() for k, v in image.items()}
+    return image
 
-classes = test_dataset.classes
 
-inputs = processor(text=classes, images=image, return_tensors="pt")
+classes_processed = processor(text=classes, return_tensors="pt")
+classes_processed.to(device)
 
-# print(inputs)
+test_dataset = CIFAR100(
+    root="./data", train=False, download=True, transform=transform_image_ALIGN
+)
 
-with torch.no_grad():
-    outputs = model(**inputs)
 
-# this is the image-text similarity score
-logits_per_image = outputs.logits_per_image
+test_dataloader = DataLoader(
+    test_dataset, batch_size=8, shuffle=True, num_workers=4, pin_memory=True
+)
 
-# we can take the softmax to get the label probabilities
-probs = logits_per_image.softmax(dim=1)
+
+print(classes_processed)
+eval_accuracy = 0
+steps = 1
+for data in tqdm(test_dataloader):
+    inputs = data[0]
+    inputs["pixel_values"] = inputs["pixel_values"].to(device)
+    classes_processed.to(device)
+    inputs.update(classes_processed)
+
+    with torch.no_grad():
+        outputs = model(**inputs)
+
+    label = data[1].to(device)
+
+    # this is the image-text similarity score
+    logits_per_image = outputs.logits_per_image
+
+    probs = logits_per_image.softmax(dim=1)
+
+    temp_eval_accuracy = torch.sum(torch.argmax(probs, dim=1) == label) / len(label)
+
+    eval_accuracy += temp_eval_accuracy
+    if steps % 100 == 0:
+        tqdm.write(f"Accuracy: {eval_accuracy / steps}")
+    steps += 1
+
+print(eval_accuracy / len(test_dataset))
 
 
 print("predicted class:", classes[torch.argmax(probs).item()])
